151f5f4bffdb37ca618e9b23396689a9ee0669f8
intensifier <christopher.kullenberg@gmail.com>
2015-08-12 15:26:32 +0200

    Adding the keywordcloud
    and updating the realm

--- README.md
+++ README.md
@@ -5,4 +5,8 @@ WOSyears.py
 - Extracts publication years from an ISI / Web of Science file and creates a histogram as a time series. 
 - Usage: python WOSyears.py file
 
+WOSkeywordcloud.py
+- Generates a wordcloud from a ISI / Web of Science file based on the Author Keywords (DE)
+- Requires the wordcloud package - https://github.com/amueller/word_cloud
+- Usage: python WOSkeywordcloud.py file
 
---
+++ WOSkeywordcloud.py
@@ -0,0 +1,66 @@
+# -*- coding: utf8 -*-
+
+"""
+This program reads a Thomson Reuters Web of Science(TM) file
+and creates a wordcloud for articles published
+It should work with any file following the ISI format.
+"""
+
+from sys import argv
+import numpy as np
+import re
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud, STOPWORDS
+
+# Printing an error message in case the user forgets filename.
+print '-' * 10
+print 'USAGE: python WOSkeywordcloud.py filename'
+print '_' * 10
+
+# Opening and reading the file selected by the user.
+script, filename = argv
+f = open(filename)
+wosrecords = f.read()
+
+text = []
+
+# Counts the articles in the dataset.
+# Number of articles (PT) should match years (PY).
+def articlecount():
+    result = re.findall(r'^PT', wosrecords, flags = re.MULTILINE)
+    articles = len(result)
+    return articles
+
+# Defining the regular expression to extract the DE field (keywords).
+def findkeywords():
+    result = re.findall(r'^DE(.*)', wosrecords, flags = re.MULTILINE)
+    text.append(result)
+
+findkeywords()
+
+# Converting the "text" list above to one single string.
+for t in text:
+    textstring = ''.join(t)
+    print textstring
+
+# Printing a message for the impatient user. 
+print '-' * 10
+print 'Creating wordcloud (this may take some time)...'
+print '-' * 10
+
+# Generating wordcloud
+wordcloud = WordCloud(
+        background_color="white", 
+        max_words=2000, 
+        stopwords=STOPWORDS,
+        width=2000,
+        height=1000
+        )
+wordcloud.generate(textstring)
+
+# Generating the image and showing it.  
+plt.imshow(wordcloud)
+plt.axis("off")
+print 'Saving as wordcloud.png...\n Please close the window to return to terminal.'
+plt.savefig("wordcloud.png") # change to .svg, .pdf etc. for other outputs. 
+plt.show()
